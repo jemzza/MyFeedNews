43281bb2e08314b21632427a09e70e93d8bdc9f3
Alexander Litvinov <allidemocracy@gmail.com>
2020-03-02 23:31:39 +0300

    Initial Commit

---
+++ MyFeedNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFeedNews
+//
+//  Created by Alexander on 02.03.2020.
+//  Copyright © 2020 Alexander Litvinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
